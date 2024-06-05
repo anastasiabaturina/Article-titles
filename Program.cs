a79eb787d242ec8d4f44f9f18997b566c96c71fe
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-06-05 13:05:25 +0300

    convert to article title

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(ArticleTitlesConverter.ConvertToArticleTitle("В шаге ОТ победы", "в от"));
+
+        Console.WriteLine(ArticleTitlesConverter.ConvertToArticleTitle("the clash of CLANS", "a an the of"));
+
+        Console.WriteLine(ArticleTitlesConverter.ConvertToArticleTitle("первый аргумент"));
+    }
+}
