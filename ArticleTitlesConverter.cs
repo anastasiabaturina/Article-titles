b1af382db4e76f1aef7e39b6fba70ded2278e860
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-06-05 14:29:52 +0300

    corrected

--- ArticleTitlesConverter.cs
+++ ArticleTitlesConverter.cs
@@ -1,9 +1,6 @@
-﻿using System.Globalization;
-using System.Linq;
-
-public class ArticleTitlesConverter
+﻿public class ArticleTitlesConverter
 {
-    public static string ConvertToArticleTitle(string input, string exceptions)
+    public static string ConvertToArticleTitle(string input, string exceptions = "")
     {
         var wordsInput = input.Split(' ');
         var wordsException = exceptions.Split(' ');
@@ -15,12 +12,14 @@ public class ArticleTitlesConverter
             {
                 result += char.ToUpper(wordsInput[i][0]) + wordsInput[i].Substring(1) + " ";
             }
+
             else
             {
                 if (wordsException.Contains(wordsInput[i].ToLower()))
                 {
                     result += wordsInput[i].ToLower() + " ";
                 }
+
                 else
                 {
                     result += char.ToUpper(wordsInput[i][0]) + wordsInput[i].Substring(1).ToLower() + " ";
@@ -30,25 +29,4 @@ public class ArticleTitlesConverter
 
         return result.Trim();
     }
-
-    public static string ConvertToArticleTitle(string input)
-    {
-        var wordsInput = input.Split(' ');
-
-        string result = "";
-        for (int i = 0; i < wordsInput.Length; i++)
-        {
-            if (i == 0)
-            {
-                result += char.ToUpper(wordsInput[i][0]) + wordsInput[i].Substring(1) + " ";
-            }
-            else
-            {
-                result += char.ToUpper(wordsInput[i][0]) + wordsInput[i].Substring(1).ToLower() + " ";
-            }
-        }
-
-        return result.Trim();
-    }
-
 }
